c4475941f5ac95f1b3811b599c9e9c36dcc1ecb5
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-03-01 23:21:34 -0500

    Basic benchmarks

---
+++ ResultsDotNet.Bench/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using BensWorkbench.Models;
+using TraceReloggerLib;
+
+namespace MyBenchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ExceptionBenches>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class ExceptionBenches
+    {
+        [Benchmark]
+        public void ThrowException()
+        {
+            string s = null!;
+            try
+            {
+                StringTestException(s);
+            }
+            catch
+            {
+                Console.Write("");
+            }
+        }
+
+        [Benchmark]
+        public void ReturnException()
+        {
+            string s = null!;
+            var results = StringTestResult(s) switch
+            {
+                var errResult when errResult.IsErr<Exception>() => true
+            };
+
+            if (results)
+            {
+                Console.Write("");
+            }
+        }
+
+        public Result<bool> StringTestResult(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return new Exception("False");
+            }
+
+            return true;
+        }
+
+        public bool StringTestException(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                throw new Exception("False");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ResultsDotNet.Lib/Result.cs
@@ -0,0 +1,171 @@
+namespace BensWorkbench.Models;
+
+/// <summary>
+/// Class which encapsulates function results as either a success type <typeparamref name="T"/>, 
+/// or some error type which extends Exception <typeparamref name="E"/>
+/// </summary>
+/// <typeparam name="T">Type for object which represents success</typeparam>
+public class Result<T> : IEquatable<T>
+{
+
+    /// <summary>
+    /// Backing store for Exception object
+    /// </summary>
+    private Exception? ErrorObject { get; }
+
+    /// <summary>
+    /// Backing store for the success value object
+    /// </summary>
+    private T? ValueInternal { get; }
+
+    /// <summary>
+    /// Constructor used when returning a successful value from any function which returns a Result<typeparamref name="T"/>>
+    /// </summary>
+    /// <param name="obj"></param>
+    public Result(T obj)
+    {
+        ValueInternal = obj;
+    }
+
+    /// <summary>
+    /// Constructor used when returning an error from any function which returns a Reuslt<typeparamref name="T"/>>
+    /// </summary>
+    /// <param name="exception"></param>
+    public Result(Exception exception)
+    {
+        ErrorObject = exception;
+    }
+
+    /// <summary>
+    /// Shortcut to get the success value object from Result<typeparamref name="T"/>>
+    /// </summary>
+    /// <returns></returns>
+    public T Unwrap()
+    {
+        if (ErrorObject is not null)
+            throw ErrorObject;
+
+
+        return ValueInternal ??
+          throw (ErrorObject ?? new Exception("Attempted to Unwrap an unused Result"));
+    }
+
+    /// <summary>
+    /// Check to see if the result is error
+    /// </summary>
+    /// <returns></returns>
+    public bool IsErr()
+    {
+        return ErrorObject is not null;
+    }
+
+    /// <summary>
+    /// Check to see if the result is an exception of type <typeparamref name="F"/>
+    /// </summary>
+    /// <typeparam name="F"></typeparam>
+    /// <returns></returns>
+    public bool IsErr<F>() where F : Exception
+    {
+        if (ErrorObject is null)
+            return false;
+
+        return typeof(F).Equals(ErrorObject!.GetType());
+    }
+
+    /// <summary>
+    /// Check to see if the result is a success value
+    /// </summary>
+    /// <returns></returns>
+    public bool IsOK()
+    {
+        return ErrorObject is null && ValueInternal is not null;
+    }
+
+    /// <summary>
+    /// For casting from <typeparamref name="T"/> object to Result<typeparamref name="T"/>>
+    /// </summary>
+    /// <param name="obj"></param>
+    public static implicit operator Result<T>(T obj) => new(obj);
+
+    /// <summary>
+    /// For casting from any Exception to Result<typeparamref name="T"/>>
+    /// </summary>
+    /// <param name="exception"></param>
+    public static implicit operator Result<T>(Exception exception) => new(exception);
+
+    /// <summary>
+    /// For assigning the value of Result<typeparamref name="T"/>>, when successful to an object of type <typeparamref name="T"/>
+    /// </summary>
+    /// <param name="rslt"></param>
+    public static implicit operator T(Result<T> rslt) => rslt.Unwrap();
+
+    /// <summary>
+    /// Allow for direct comparisons between the success object, and an object of the same type
+    /// </summary>
+    /// <param name="obj1"></param>
+    /// <param name="obj2"></param>
+    /// <returns></returns>
+    public static bool operator ==(Result<T> obj1, T obj2)
+    {
+        return obj1.Equals(obj2);
+    }
+
+    /// <summary>
+    /// Allow for direct comparisons between the success object, and an object of the same type
+    /// </summary>
+    /// <param name="obj1"></param>
+    /// <param name="obj2"></param>
+    /// <returns></returns>
+    public static bool operator !=(Result<T> obj1, T obj2)
+    {
+        return obj1.Equals(obj2);
+    }
+
+    /// <summary>
+    /// Allow for direct comparisons between the internal error, and an Exception
+    /// </summary>
+    /// <param name="obj1"></param>
+    /// <param name="obj2"></param>
+    /// <returns></returns>
+    public static bool operator ==(Result<T> obj1, Exception obj2)
+    {
+        try
+        {
+            obj1.Unwrap();
+            return false;
+        }
+        catch (Exception exception)
+        {
+            return exception.Message == obj2.Message;
+        }
+    }
+
+    /// <summary>
+    /// Allow for direct comparisons between the internal error, and an Exception
+    /// </summary>
+    /// <param name="obj1"></param>
+    /// <param name="obj2"></param>
+    /// <returns></returns>
+    public static bool operator !=(Result<T> obj1, Exception obj2)
+    {
+        try
+        {
+            obj1.Unwrap();
+            return true;
+        }
+        catch (Exception exception)
+        {
+            return exception.Message != obj2.Message; ;
+        }
+    }
+
+    /// <summary>
+    /// To satisfy Interface
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(T? other)
+    {
+        return other?.Equals(ValueInternal) ?? false;
+    }
+}
